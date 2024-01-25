a05b1c430a50f0ab23fe9330903486e8afa510f9
SongHae8640 <thdgo456@naver.com>
2024-01-25 13:53:33 +0900

    etc : init project

---
+++ src/test/java/com/seasonalfood/SeasonalFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seasonalfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeasonalFoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
